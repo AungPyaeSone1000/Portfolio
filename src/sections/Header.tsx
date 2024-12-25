9bd29c64d6dd260cb5143aeadacaa84d338e95f8
Aung Pyae Sone <ww.aps2002@gmail.com>
2024-12-25 08:30:58 -0500

    Header setup

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,3 +1,20 @@
 export const Header = () => {
-  return <div>Header Section</div>;
+  return (
+    <div className="flex justify-center items-center fixed top-3 w-full">
+      <nav className="flex gap-1 p-0.5 bg-white/10 border border-yellow-400 backdrop-blur rounded-full">
+        <a href="#" className="nav-item">
+          Home
+        </a>
+        <a href="#" className="nav-item">
+          Projects
+        </a>
+        <a href="#" className="nav-item">
+          About
+        </a>
+        <a href="#" className="nav-item ">
+          Contact
+        </a>
+      </nav>
+    </div>
+  );
 };
