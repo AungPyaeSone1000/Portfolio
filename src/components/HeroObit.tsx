a87c0e01d2a01b89a17edf377a9d75cf719efd57
Aung Pyae Sone <ww.aps2002@gmail.com>
2025-01-08 00:18:34 -0500

    Spin animate

--- src/components/HeroObit.tsx
+++ src/components/HeroObit.tsx
@@ -1,27 +1,49 @@
 import { PropsWithChildren } from "react";
+import { twMerge } from "tailwind-merge";
 
 const HeroObit = ({
   children,
   size,
   rotation,
-}: PropsWithChildren<{ size: number; rotation: number }>) => {
+  obit = false,
+  spin = false,
+  obitDuration,
+  spinDuration,
+}: PropsWithChildren<{
+  size: number;
+  rotation: number;
+  obit?: boolean;
+  spin?: boolean;
+  obitDuration?: string;
+  spinDuration?: string;
+}>) => {
   return (
-    <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+    <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 -z-20">
       <div
-      className="flex items-start justify-start"
-        style={{
-          transform: `rotate(${rotation}deg)`,
-          width: `${size}px`,
-          height: `${size}px`,
-        }}
+        className={twMerge(obit === true && "animate-spin")}
+        style={{ animationDuration: obitDuration }}
       >
         <div
-          className="inline-flex"
+          className="flex items-start justify-start"
           style={{
-            transform: `rotate(${rotation * -1}deg)`,
+            transform: `rotate(${rotation}deg)`,
+            width: `${size}px`,
+            height: `${size}px`,
           }}
         >
-          {children}
+          <div
+            className={twMerge(spin === true && "animate-spin")}
+            style={{ animationDuration: spinDuration }}
+          >
+            <div
+              className="inline-flex"
+              style={{
+                transform: `rotate(${rotation * -1}deg)`,
+              }}
+            >
+              {children}
+            </div>
+          </div>
         </div>
       </div>
     </div>
