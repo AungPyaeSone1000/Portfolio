fdf04842f79be7a71522c6e9df9a20df41f0845e
Aung Pyae Sone <ww.aps2002@gmail.com>
2025-01-14 16:20:00 -0500

    Header position fixed

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,6 +1,6 @@
 export const Header = () => {
   return (
-    <div className="flex justify-center items-center relative top-3 z-10">
+    <div className="flex justify-center items-center fixed top-3 w-full z-10">
       <nav className="flex gap-1 p-0.5 bg-white/10 border border-yellow-400 backdrop-blur rounded-full">
         <a href="#" className="nav-item">
           Home
